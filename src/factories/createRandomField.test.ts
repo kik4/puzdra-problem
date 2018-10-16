4a5ced651f6491c20278f33d9b0bbb8d9533b8f8
kik4 <7163720+kik4@users.noreply.github.com>
2018-10-17 01:06:52 +0900

    refactor test createRandomField.test

--- src/factories/createRandomField.test.ts
+++ src/factories/createRandomField.test.ts
@@ -4,9 +4,9 @@ import createRandomBox from "./createRandomBox"
 describe("createRandomField", () => {
   it("createRandomField()'s size to be 6x5", () => {
     const field = createRandomField(createRandomBox)
-    expect(field.length).toEqual(5)
+    expect(field).toHaveLength(5)
     field.forEach(line => {
-      expect(line.length).toEqual(6)
+      expect(line).toHaveLength(6)
     })
   })
 })
