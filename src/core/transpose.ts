bc7dde04d24013b63b4182e89b7b5e51a321463b
kik4 <7163720+kik4@users.noreply.github.com>
2018-10-20 23:29:48 +0900

    generics transpose

--- src/core/transpose.ts
+++ src/core/transpose.ts
@@ -1,1 +1,1 @@
-export default (a: Array<Array<any>>): Array<Array<any>> => a[0].map((_, c) => a.map(r => r[c]))
+export default <T = any>(a: Array<Array<T>>): Array<Array<T>> => a[0].map((_, c) => a.map(r => r[c]))
